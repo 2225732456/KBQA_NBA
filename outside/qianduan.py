4bff0a580c54b1b5237683adab3d8924b2f5ab54
SuperLA <52823169+2225732456@users.noreply.github.com>
2022-03-03 14:31:34 +0800

    Add files via upload

---
+++ outside/qianduan.py
@@ -0,0 +1,12 @@
+import eel
+import kbqa_test
+# 定义html文件所在文件夹名称
+eel.init('web')
+@eel.expose # 使用装饰器,类似flask里面对路由的定义
+def answer(key):
+    mykbqa = kbqa_test.KBQA(key)
+    content = mykbqa.main()
+    print(key)
+    print(content)
+    return content
+eel.start('index.html', port=0, size=(600,300))
